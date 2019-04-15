476cd52c3440d734a7844c3afdf500c119b99109
fabjordan <fabriciomonte2014@gmail.com>
2019-04-15 15:42:47 -0300

    First Commit
    File to mysql dump in container docker

---
+++ docker-mysql-dump.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+data=$(date '+%d-%m-%Y_%H-%M-%S');
+nome='backup_'$data.sql;
+
+# Caso não tenha o Figlet instalado basta rodar: sudo apt install figlet
+figlet -c EXECUTANDO BACKUP
+
+echo "----------------------------------------------------";
+echo "| ACESSANDO SERVIDOR REMOTO E REALIZANDO DUMP MYSQL |";
+echo "----------------------------------------------------";
+
+ssh user@ipServidor "docker exec nome-container mysqldump -u root --password='senha-do-banco' nome-do-banco | gzip -v > /local-onde-ficara-o-dump-mysql/$nome.gz";
+
+echo "\n------------------------------------"
+echo "| MYSQL DUMP REALIZADO COM SUCESSO! |";
+echo "------------------------------------"
+
+echo "\nFAZENDO DOWNLOAD...";
+
+rsync -avz -e ssh user@ipServidor:/local-onde-ficara-o-dump-mysql/$nome.gz --progress ~/minha-pasta-local
+
+echo "\n-------------------------------------------------------------------";
+echo "| DOWNLOAD $nome FINALIZADO COM SUCESSO! |";
+echo "-------------------------------------------------------------------";
+
+figlet -c CONCLUIDO COM SUCESSO!
